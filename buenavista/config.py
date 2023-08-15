216d8e6afc3112ed163482ed901f642a330c6613
Husain Khamis <64447087+actuary87@users.noreply.github.com>
2023-08-15 22:09:07 +0300

    Creating a configuration file

---
+++ buenavista/config.py
@@ -0,0 +1,9 @@
+# real Postgres connection info
+real_postgres_server_address = "localhost"
+real_postgres_server_port = 5432
+real_postgres_server_user = "postgres"
+real_postgres_server_password = "postgres"
+database_name = "test"
+
+# postgres_proxy_server_port must be different than the real_postgres_server_port
+postgres_proxy_server_port = 5433
--- buenavista/examples/postgres_proxy.py
+++ buenavista/examples/postgres_proxy.py
@@ -1,17 +1,18 @@
-import os
+import buenavista.config as config
 
 from buenavista.postgres import BuenaVistaServer
 from buenavista.backends.postgres import PGConnection
 
-address = ("localhost", 5433)
+address = ("localhost", config.postgres_proxy_server_port)
 server = BuenaVistaServer(
     address,
     PGConnection(
         conninfo="",
-        host="localhost",
-        port=5432,
-        user=os.getenv("USER"),
-        dbname="postgres",
+        host=config.real_postgres_server_address,
+        port=config.real_postgres_server_port,
+        user=config.real_postgres_server_user,
+        password=config.real_postgres_server_password,
+        dbname=config.database_name,
     ),
 )
 ip, port = server.server_address
